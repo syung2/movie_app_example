effa8c62748bc697d057724c6bf5b4e498d0bd4d
kskim8087 <kskim@rodaloft.com>
2020-09-22 12:13:39 +0900

    Updates

---
+++ precache-manifest.2a78ab770c3be3462f4c0d32bcea4a5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3324361a6c5f690889384c82a850457",
+    "url": "/movie_app_example/index.html"
+  },
+  {
+    "revision": "1355050a0f580e9a6995",
+    "url": "/movie_app_example/static/css/main.ab5b79a6.chunk.css"
+  },
+  {
+    "revision": "4d7c806d0e258b226914",
+    "url": "/movie_app_example/static/js/2.00a09d16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_example/static/js/2.00a09d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1355050a0f580e9a6995",
+    "url": "/movie_app_example/static/js/main.546963dc.chunk.js"
+  },
+  {
+    "revision": "c0f456deb3b6fc95f576",
+    "url": "/movie_app_example/static/js/runtime-main.4e1ddca6.js"
+  }
+]);
